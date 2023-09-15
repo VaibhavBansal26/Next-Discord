a19ae471a19c2c8bfed3339f713176a16a147ce0
Vaibhav Bansal <vaibhav.bansal945@gmail.com>
2023-09-15 23:53:29 +0530

    basic setup

---
+++ app/(auth)/(routes)/register/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+const Register = () => {
+    return (
+    <div>
+        This is registeration page
+    </div>);
+}
+ 
+export default Register;
---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const AuthLayout = ({children}:{children:React.ReactNode}) => {
+    return (
+    <div className="bg-green-500 h-full">
+        {children}
+    </div>);
+}
+ 
+export default AuthLayout;
